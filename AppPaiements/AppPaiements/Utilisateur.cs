ef9c8fe566629e40606c6bf316e2147387abef90
Cecilia Inoa <300139282@monboreal.ca>
2024-10-22 16:54:31 -0400

    Création de la classe utilisateur

---
+++ AppPaiements/AppPaiements/Utilisateur.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur 
+    {
+        public string Nom { get; set; }
+        public List<Paiement> paiements { get; set; }
+        public Utilisateur(string nom) 
+        {
+            Nom = nom;
+            paiements= new List<Paiement>();
+        }
+        
+        public void AfficherDetails()
+        {
+            Console.WriteLine($"Nom: {Nom}");
+
+        }
+        public void AjouterPaiement(Paiement paiement)
+        {
+           
+                paiements.Add(paiement); }
+    }
+}
+
+
